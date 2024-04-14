536478d431e1ef763b48259d4b69717e6284a831
sayel <mdsayel111@gmail.com>
2024-04-15 01:55:51 +0600

    deploy in vercel

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,9 @@ app.use("/auth", authRouter)
 app.use("/user", userRouter)
 app.use("/message", messageRouter)
 app.use("/conversation", conversationRouter)
+app.get("/",async (req: Request, res: Response) => {
+    res.send({message: "hellow world"})
+})
 
 app.listen(port, async () => {
     await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@chat-app-db.hk0ts7w.mongodb.net/chat-app-DB?retryWrites=true&w=majority&appName=chat-app-DB`);
